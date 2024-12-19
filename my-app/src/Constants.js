397edd9554acc23cd750263fd56f3f9d901a4e0b
Zeyad Ayman <145010313+ZeyadAy-man@users.noreply.github.com>
2024-12-20 01:13:21 +0200

    Delete my-app directory

--- my-app/src/Constants.js
+++
@@ -1,7 +0,0 @@
-const primaryColor = "#060b26"; //black: used with text, navBar, &footer
-const secondaryColor = '#1a83ff'; //blue: used with hovering, buying buttons 
-const thirdColor = '#f5f5f5'; //white: used with main background
-const fourthColor = '#f0f0f0'; //lightGray: used with background components such as user, laptop, &order component
-export {primaryColor, secondaryColor, thirdColor, fourthColor}
-
-//Goodluck!
